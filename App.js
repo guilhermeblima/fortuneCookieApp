c5cbff2f9989c111409a4c859dee062db90ca80c
Guilherme Lima <guilhermelima.iba@gmail.com>
2019-02-17 21:41:39 +1300

    implementation

--- App.js
+++ App.js
@@ -1,38 +1,93 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+class CustomButton extends Component{
+  constructor(props){
+    super(props);
+    this.state = '';
+
+    this.styles = StyleSheet.create({
+      button:{
+        width: 230, 
+        height: 50,
+        borderColor: props.color, 
+        borderWidth: 2,
+        borderRadius: 25
+      }, 
+      btnArea:{
+        flex: 1,
+        flexDirection: 'row', 
+        justifyContent: 'center',
+        alignItems: 'center'
+
+      }, 
+      btnText:{
+        fontSize: 17,
+        fontWeight: 'bold',
+        color: props.color
+
+      }
+
+    });
+
+  }
+
+  render(){
+    return(
+      <TouchableOpacity style={this.styles.button} onPress={this.props.onPress}>
+        <View style={this.styles.btnArea}>
+          <Text style={this.styles.btnText}>{this.props.buttonText}</Text>
+        </View>
+
+      </TouchableOpacity>
+    );
+  }
+}
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      message: ''
+    };
+
+    this.messages = [
+      'A friend asks only for your time not your money.',
+      'A smile is your passport into the hearts of others.',
+      'Your high-minded principles spell success.',
+      'Change can hurt, but it leads a path to something better.',
+      'People are naturally attracted to you.',
+      'A chance meeting opens new doors to success and friendship.',
+      'You learn from your mistakes... You will learn a lot today.',
+      'If you have something good in your life, dont let it go!',
+      'Your shoes will make you happy today.',
+      'A dream you have will come true.',
+      'You will become great if you believe in yourself.'
+    ];
+
+    this.breakOpen = this.breakOpen.bind(this);
+  }
+
+  breakOpen(){
+    let randomIndex = Math.floor(Math.random() * this.messages.length);
+    this.setState({message: this.messages[randomIndex]});
+  }
   render() {
     return (
+      
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Image source={require('./src/biscoito.png')} style={styles.image}/>
+        <Text style={styles.message}>{this.state.message}</Text>
+        <CustomButton color="#dd7b22" buttonText="break open fortune cookie" onPress={this.breakOpen}/>
       </View>
     );
   }
@@ -41,18 +96,19 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 20,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  image:{
+    width: 250, 
+    height: 250,
+  }, 
+  message:{
+    color:'#dd7b22', 
+    margin: 30,
+    fontSize: 20, 
+    textAlign: 'center'
+  }
+
 });
